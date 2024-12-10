51bc5f55d8d6b46fcbb3f892afd7443652e56a4a
Anderson Gabriel <gabrielvalencasa98@gmail.com>
2024-12-10 15:21:17 -0300

    feat: integrate Stripe for payment processing and enhance subscription page layout
    - Added Stripe dependencies: `@stripe/stripe-js` and `stripe` to `package.json` and `package-lock.json`.
    - Enhanced the subscription page with a new layout featuring Basic and Premium plans, including pricing and transaction details.
    - Implemented card components for better visual representation of subscription options.
    - Ensured user authentication is checked before rendering subscription options.

--- app/subscription/page.tsx
+++ app/subscription/page.tsx
@@ -1,18 +1,77 @@
-
 import { redirect } from "next/navigation";
 import Navbar from "../_components/ui/navbar";
 import { auth } from "@clerk/nextjs/server";
+import {
+  Card,
+  CardContent,
+  CardHeader,
+} from "../_components/ui/card";
+import { CheckIcon, XIcon } from "lucide-react";
+import AcquirePlanButton from "./_components/acquire-plan-button";
 
-const subscription = async() => {
-    
-    const { userId } = await auth();
-    if (userId) {
-      redirect("/");
-    }
-    
-    return ( 
-    <Navbar/>
+const subscription = async () => {
+  const { userId } = await auth();
+  if (!userId) {
+    redirect("/login");
+  }
+
+
+  return (
+    <>
+      <Navbar />
+      <div className="space-y-6 p-6">
+        <h1 className="font-bold text-2xl">Assinatura</h1>
+        <div className="flex gap-6">
+          <Card className="w-[450px]">
+            <CardHeader className="border-b border-solid py-8">
+              <h2 className="text-center text-2xl font-semibold">
+                Plano Básico
+              </h2>
+              <div className="flex items-center gap-3 justify-center">
+                <span className="text-4xl">R$</span>
+                <span className="text-6xl font-semibold">0</span>
+                <span className="text-2xl text-muted-foreground">/mês</span>
+              </div>
+            </CardHeader>
+            <CardContent className="space-y-8 py-8">
+              <div className="flex items-center gap-2">
+                <CheckIcon className="text-primary" />
+                <p>Apenas 10 trnasações por mês (7/10)</p>
+              </div>
+              <div className="flex items-center gap-2">
+                <XIcon className="text-" />
+                <p>Relatórios de IA</p>
+              </div>
+            </CardContent>
+          </Card>
+
+          <Card className="w-[450px]">
+            <CardHeader className="border-b border-solid py-8">
+              <h2 className="text-center text-2xl font-semibold">
+                Plano Premium
+              </h2>
+              <div className="flex items-center gap-3 justify-center">
+                <span className="text-4xl">R$</span>
+                <span className="text-6xl font-semibold">19</span>
+                <span className="text-2xl text-muted-foreground">/mês</span>
+              </div>
+            </CardHeader>
+            <CardContent className="space-y-8 py-8">
+              <div className="flex items-center gap-2">
+                <CheckIcon className="text-primary" />
+                <p>Transações ilimitadas</p>
+              </div>
+              <div className="flex items-center gap-2">
+                <CheckIcon className="text-primary" />
+                <p>Relatórios de IA</p>
+              </div>
+              <AcquirePlanButton />
+            </CardContent>
+          </Card>
+        </div>
+      </div>
+    </>
   );
-}
- 
+};
+
 export default subscription;
